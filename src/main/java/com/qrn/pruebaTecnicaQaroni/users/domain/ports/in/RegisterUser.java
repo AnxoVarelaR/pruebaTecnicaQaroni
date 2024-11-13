99e222c244f4c15e6a94b3653bb17f248f1089cf
Anxo Varela <anxovarela@Anxos-MacBook-Pro.local>
2024-11-13 10:17:56 +0100

    PDF respuestas teoria

---
+++ src/main/java/com/qrn/pruebaTecnicaQaroni/users/domain/ports/in/RegisterUser.java
@@ -0,0 +1,8 @@
+package com.qrn.pruebaTecnicaQaroni.users.domain.ports.in;
+
+import com.qrn.pruebaTecnicaQaroni.users.domain.User;
+
+public interface RegisterUser {
+    User registerUser(User user);
+}
+    
